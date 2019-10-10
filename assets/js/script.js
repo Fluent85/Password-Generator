b6cf5627ae484572a4c83a994f1a8642cc27eb92
Fluent85 <tshabalala.richard@gmail.com>
2019-10-09 20:12:19 -0500

    updated the JavaScript file to make generate password function work and copy password function work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,55 @@
 window.confirm ("Are you ready to generate a password?");
+var length = 0;
+while(length < 8 || length > 128){
+    length = prompt("Please enter length of password", 8);
+    length = parseInt(length, 10);
+}
+var specialCharacter = confirm("Special characters?");
+var numericCharacter = confirm("Numeric character?");
+var lowercaseCharacter = confirm("Lowercase character?");
+var uppercaseCharacter = confirm("Uppercase character?");
 
 var generate = "Generate Password";
-var copy = "Copy to Clipboard";
+
+var clipboard = "Copy to Clipboard";
+var values = "";
 
 //generate password
-function generate(){
+function makePassword(){
+    if (specialCharacter){
+        values += "/^[-_@.#&+-]*$";
+    }
+    if (numericCharacter){
+        values += "0123456789";
+    }
+    if (lowercaseCharacter){
+        values += "zxcvbnmlkjhgfdsaqwertyuiop";
+    }
+    if (uppercaseCharacter){
+        values += "ZXCVBNMASDFGHJKLPOIUYTREWQ";
+    }
+    let password = "";
 
+    //create for loop to select password characters
+    for(var i= 0; i < length; i++){
+        password = password + values.charAt(Math.floor(Math.random() * values.length - 1));
+    }
+    return password;
 }
 
-//set password length and complexity
-let complexity = document.getElementById("generate").values;
-
-//possible values
-const values = "/^[ A-Za-z0-9_@./#&+-]*$/";
-
-let password = "";
-
-//create for loop to select password characters
-for(var i= 0; i <= complexity; i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-}
-
-//set default length of 8
-document.getElementById("generate").innerHTML = "length 8";
-
-//add password to textbox area
-document.getElementById("password").value = password;
+function myFunction() {
+    document.getElementById("password").innerHTML = makePassword();
+  }
 
 //function to copy password to clipboard
-function copyPassword(){
-    document.getElementById("copy").select();
-    
+function copyPassword() {
+    var clipboard = document.getElementById("password");
+    clipboard.select();
+
+    var coppiedPassword = clipboard.textContent;
+        console.log(clipboard);
+        console.log(coppiedPassword);
     document.execCommand("copy");
 
-    alert("Password is copied to clipboard!");
+    alert("Password is copied to clipboard!" + clipboard.value);
 }
